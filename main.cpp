b3d27844202efa4764f1f115d07ab24d63e6bddb
Jone J Jabari <jonejabari123@gmail.com>
2024-06-20 14:53:03 +0200

    update

--- main.cpp
+++ main.cpp
@@ -5,147 +5,149 @@
 
 using namespace std;
 
-int main()
-{
-    int a,i = 0;
-    string text, old, password1, password2, pass, name, passwaord0, age, user, word, word1;
-    string creds[2], cp[2];
+void registerUser() {
+    string name, password0, age;
+    cout << "_______________________________________" << endl << endl;
+    cout << "|-----------------Register ------------|" << endl;
+    cout << "|______________________________________|" << endl << endl;
+    cout << "Enter user name: ";
+    cin >> name;
+    cout << "Enter password: ";
+    cin >> password0;
+    cout << "Enter your age: ";
+    cin >> age;
 
-    cout <<"     Security System  "<< endl;
-
-    cout <<"______________________________________"<<endl<<endl;
-    cout <<"|            1. Register               |"<<endl;
-    cout <<"|            2. Login                  |"<<endl;
-    cout <<"|            3. Change Password        |"<<endl;
-    cout <<"|__________  4. End Program __________ |"<<endl<<endl;
-
-    do{
-        cout<<endl<<endl;
-        cout<<"Enter your choice: ";
-        cin>>a;
-        switch(a)
-        {
-            case 1:{
-                cout<<"________________________________________"<<endl<<endl;
-                cout<<"|-----------------Register -------------|"<<endl;
-                cout<<"|_______________________________________|"<<endl<<endl;
-                cout<<"Enter user name:-";
-                cin>>name;
-                cout<<"Enter password:-";
-                cin>>passwaord0;
-                cout<<"Enter your age:-";
-                cin>>age;
-
-                ofstream of1;
-                of1.open("file.text");
-                if(of1.is_open()){
-                    of1<<name<<"\n";
-                    of1<<password0;
-                    cout<<"registration successful"<<endl;
-                }
-
-
-            break;
-            }
-            case 2:{
-                i=0;
-
-                cout<<"___________________________________"<<endl<<endl;
-                cout<<"|----------------Login-------------|"<<endl;
-                cout<<"|__________________________________|"<endl<<endl;
-
-                ifstream of2;
-                of2.open("file.text";)
-                cout<<"Enter user name:-"
-                cin>>user;
-                cout<<"enter password";
-                cin>>pass;
-
-                if(of2.is_open()){
-                    while(!of2.eof()){
-                            while(getline(of2,text)){//using this get line file will be read line by line
-                            istringstream iss(text);
-                            iss>>word;
-                            creds[i]=word;
-                            i++;
-                        }
-
-                        if(user==creds[0] && pass==creds[1]){
-                            cout<<"----Log in successful-----";
-                            cout<<endl<<endl;
-
-                            cout<<" Details: "<<endl;
-
-                            cout<<"User name: "+ name<<endl;
-                            cout<<"Password: "+ pass<<endl;
-                            cout<<"Age: "+ age<<endl;
-                        }
-                        else{
-                            cout<<endl<<endl;
-                            cout<<"incorrect Credentials"<<endl;
-                            cout<<"|     1. Press 2 to login           |"<<endl;
-                            cout<<"|     2. Press 3 to change paaword  |"<<endl;
-                            break;
-                        }
-                    }
-                }
-                break;
-            }
-
-            case 3:{
-            i=0;
-
-            cout<<"------------Change password----------"<<endl;
-
-            ifstream of0;
-            of0.open("file.text");
-            cout<<"enter old password:-";
-            cin>>old;
-            if(of0.is_open()){
-                while(of0.eof()){
-                    while(getline(of0, text)){
-                        istringstream iss(text);
-                        iss>>word1;// here we store data in word 1
-                        cp[1]=wordl;
-                        i++;
-                    }
-                    if(old==cp[1]){
-                        of0.close();
-                        ofstream of1;
-                        if(of1.is_open()){
-                            cout<<"enter new password:-";
-                            cin>>password1;
-                            cout<<"enter your password again:-";
-                            cin>>password2;
-
-                            if(password1==password2){
-                                of1<<cp[0]<<"\n";
-                                of1<<password1;
-                                cout<<"Password changed successfully"<<endl;
-                            }
-                            else{
-                                of1<<cp[0]<<"\n";
-                                of1<<old;
-                                cout<<"Password do not match"<<endl;
-                            }
-                        }
-                    }
-                    else{
-                        cout<<"enter a valid password"<<endl;
-                        break;
-                    }
-                }
-            }
-            break;
-        }
-        case 4:{
-            cout<<"___________ Thank You!__________";
-            break;
-        }
-        default:
-            cout<<"Enter a valid choice";
+    ofstream of1;
+    of1.open("file.txt", ios::app);  // Append to the file
+    if (of1.is_open()) {
+        of1 << name << "\n";
+        of1 << password0 << "\n";
+        of1 << age << "\n";
+        of1.close();
+        cout << "Registration successful" << endl;
+    } else {
+        cout << "Error opening file" << endl;
     }
 }
-while(a!=4);
-return 0;
+
+void loginUser() {
+    string user, pass, name, password, age, line;
+    bool found = false;
+
+    cout << "__________________________________" << endl << endl;
+    cout << "|----------------Login------------|" << endl;
+    cout << "|_________________________________|" << endl << endl;
+
+    cout << "Enter user name: ";
+    cin >> user;
+    cout << "Enter password: ";
+    cin >> pass;
+
+    ifstream of2("file.txt");
+    if (of2.is_open()) {
+        while (getline(of2, name) && getline(of2, password) && getline(of2, age)) {
+            if (user == name && pass == password) {
+                found = true;
+                cout << "----Login successful-----" << endl;
+                cout << "Details:" << endl;
+                cout << "User name: " << name << endl;
+                cout << "Password: " << pass << endl;
+                cout << "Age: " << age << endl;
+                break;
+            }
+        }
+        of2.close();
+        if (!found) {
+            cout << "Incorrect Credentials" << endl;
+            cout << "|     1. Press 2 to login           |" << endl;
+            cout << "|     2. Press 3 to change password  |" << endl;
+        }
+    } else {
+        cout << "Error opening file" << endl;
+    }
+}
+
+void changePassword() {
+    string user, oldPassword, newPassword1, newPassword2, name, password, age, line;
+    bool found = false;
+
+    cout << "------------Change password----------" << endl;
+
+    cout << "Enter user name: ";
+    cin >> user;
+    cout << "Enter old password: ";
+    cin >> oldPassword;
+
+    ifstream of0("file.txt");
+    ofstream temp("temp.txt");
+    if (of0.is_open() && temp.is_open()) {
+        while (getline(of0, name) && getline(of0, password) && getline(of0, age)) {
+            if (user == name && oldPassword == password) {
+                found = true;
+                cout << "Enter new password: ";
+                cin >> newPassword1;
+                cout << "Enter your password again: ";
+                cin >> newPassword2;
+                if (newPassword1 == newPassword2) {
+                    temp << name << "\n";
+                    temp << newPassword1 << "\n";
+                    temp << age << "\n";
+                    cout << "Password changed successfully" << endl;
+                } else {
+                    temp << name << "\n";
+                    temp << password << "\n";
+                    temp << age << "\n";
+                    cout << "Passwords do not match" << endl;
+                }
+            } else {
+                temp << name << "\n";
+                temp << password << "\n";
+                temp << age << "\n";
+            }
+        }
+        of0.close();
+        temp.close();
+        remove("file.txt");
+        rename("temp.txt", "file.txt");
+        if (!found) {
+            cout << "Invalid user name or password" << endl;
+        }
+    } else {
+        cout << "Error opening file" << endl;
+    }
+}
+
+int main() {
+    int choice;
+
+    cout << "     Security System  " << endl;
+    cout << "______________________________________" << endl << endl;
+    cout << "|            1. Register               |" << endl;
+    cout << "|            2. Login                  |" << endl;
+    cout << "|            3. Change Password        |" << endl;
+    cout << "|__________  4. End Program __________|" << endl << endl;
+
+    do {
+        cout << endl << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                registerUser();
+                break;
+            case 2:
+                loginUser();
+                break;
+            case 3:
+                changePassword();
+                break;
+            case 4:
+                cout << "__________ Thank You! ___________" << endl;
+                break;
+            default:
+                cout << "Enter a valid choice" << endl;
+        }
+    } while (choice != 4);
+
+    return 0;
 }
